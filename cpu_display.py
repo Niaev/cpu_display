c3d4840a588d69c72d97fb4be22af35830e1d515
Niaev <trutnv@protonmail.com>
2022-05-12 15:50:22 -0300

    PEP 8 Friendly code

--- cpu_display.py
+++ cpu_display.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-# imports
-import drivers
+import os
+import sys
 from time import sleep
-import os, sys
+
+import drivers
 
 try:
+    # Setup section
     try:
-        # init display
+        # Init display
         display = drivers.Lcd()
     except OSError as e:
         print("[+] No LCD connected!")
@@ -16,33 +18,33 @@ try:
 
     print("[+] LCD detected!")
     print("[+] Writing information...")
-    display.lcd_backlight(1) # turn on backlight
+    display.lcd_backlight(1) # Turn on backlight
 
-    # loop
+    # Loop section
     while True:
-        # get cpu temperature and usage from os info
+        # Get cpu temperature and usage from os info
         os.system("sensors | grep temp1: > /tmp/temp.txt") 
         os.system("grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}' >> /tmp/temp.txt")
 
-        # read temporary file with values
+        # Read temporary file with values
         with open('/tmp/temp.txt','r') as f:
             text = f.read().split('\n')
-            temp = text[0].split('+')[1] # get temperature
-            usag = text[1] # get usage
+            temp = text[0].split('+')[1] # Get temperature
+            usag = text[1] # Get usage
     
-        # formatting values
+        # Formatting values
         temp = temp.replace('°','o')
         usag = '{:.2f}'.format(float(usag))
 
-        # adding spaces to the end to fit in 16 char line
+        # Adding spaces to the end to fit in 16 char line
         temp = '%-16s' % temp
         usag = '%-16s' % (usag + '%')
 
-        # display values
+        # Display values
         display.lcd_display_string(f'Temp.: {temp}',1)
         display.lcd_display_string(f'Usage: {usag}',2)
     
-        sleep(2) # wait
+        sleep(2) # Wait
 except KeyboardInterrupt as e:
     print("\n[+] Shutting down...")
     display.lcd_clear()
